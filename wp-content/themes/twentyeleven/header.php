8c5042b50f2bb3e6b267a14dfc2df71019a0c4d2
Tod Cribb <tdcribb@gmail.com>
2013-04-04 12:49:48 -0400

    added google analytics

--- wp-content/themes/twentyeleven/header.php
+++ wp-content/themes/twentyeleven/header.php
@@ -74,6 +74,20 @@
 <link rel="stylesheet" type="text/css" media="all" href="/wp-content/css/iphone.css" />
 <script src="<?php echo esc_url( home_url( '/' ) ); ?>/wp-content/js/landc.js"></script>
 
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-39857490-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
 </head>
 
 <body <?php body_class(); ?>>
